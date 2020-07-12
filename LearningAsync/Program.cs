acc6e71e04322a2036cd97b6b7259d166bcef745
marco villanueva <villanuevamarco98@gmail.com>
2020-07-11 18:28:27 -0600

    Add project files.

---
+++ LearningAsync/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Computing...");
+            Download();
+            Console.ReadLine();
+        }
+        static void Download()
+        {
+            //Inline anonymous function
+            //function embedded in Task.Run
+            //Everything in the braces is a function w/o name
+            //The task is like a helper; it is resting as readline operates
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Data assimilated");
+            });
+        }
+    }
+}
